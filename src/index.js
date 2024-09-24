57e4db14766f87fd35000111f92160a1d51340f7
Tsubasa <katakiritsubasa@gmail.com>
2024-09-24 22:34:35 +0300

    feat: add solution to brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 !== 0) return false;
+  const stack = [];
+  const openers = bracketsConfig.map(item => item[0]);
+  for(let i = 0; i < str.length; i++){
+    if(openers.includes(str[i])) {
+      const checkElement = bracketsConfig.find(item => item[0] === str[i]);
+      if(checkElement[0] === checkElement[1] && stack[stack.length - 1] === str[i]) stack.pop();
+      else stack.push(str[i]);
+    }
+    else if (stack.length === 0 && !openers.includes(str[i])) return false;
+    else {
+      const checkElement = bracketsConfig.find(item => item[0] === stack[stack.length - 1])[1];
+      if(str[i] === checkElement) stack.pop();
+      else return false;
+    }
+  }
+  if(stack.length > 0) return false;
+  return true;
 }
